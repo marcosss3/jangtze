90517028164b23e1721772732d5beab74fd077fd
Tam, Marcos (UK - London) <marcostam@deloitte.co.uk>
2020-07-05 12:29:32 +0100

    Testing

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
+const path = require("path");
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/jangtze/'
-    : '/'
+  outputDir: path.resolve(__dirname, "docs"),
 }
